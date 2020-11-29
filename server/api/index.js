f61300f9b02b6e43c69aaef17093a252535eabc1
Lidia De La Cruz <lidcruz@hotmail.com>
2020-11-29 14:43:56 -0500

    Mounted models to api index file

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,15 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/channels', require('./channels'))
+router.use('/recipe', require('./recipe'))
+router.use('/event', require('./event'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
   next(error)
 })
+
+
+module.exports = router
