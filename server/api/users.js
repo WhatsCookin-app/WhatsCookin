84e1d409f15c3fe47ff05682263c6c3517da49f4
Lidia De La Cruz <lidcruz@hotmail.com>
2020-11-30 09:43:51 -0500

    Added Routes for User

--- server/api/users.js
+++ server/api/users.js
@@ -15,3 +15,23 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.get('/:id',async(req,res,next) => {
+  try{
+    const user = await User.findByPk(req.params.id)
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async(req,res,next) => {
+  try{
+    const user = await User.create(req.body)
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+
