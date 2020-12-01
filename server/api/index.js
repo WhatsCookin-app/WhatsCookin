8ceb8b179be24e441aa8e846a46b3c141e77be01
Lidia Y De La Cruz <62112287+lidiadelacruz@users.noreply.github.com>
2020-12-01 10:41:55 -0500

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-router.use('/channels', require('./channels'))
-router.use('/recipe', require('./recipe'))
-router.use('/event', require('./event'))
+
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
