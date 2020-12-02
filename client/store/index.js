f1a699e1f22e894c2804dccde680a9623d67722a
Yufan Zhang <mikyla.c.zhang@gmail.com>
2020-12-02 17:55:53 -0500

    Feat: added single recipe component and recipe owners are able to edit the recipe

--- client/store/index.js
+++ client/store/index.js
@@ -4,10 +4,12 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import recipe from './recipe'
+import singleRecipe from './singleRecipe'
 
 const reducer = combineReducers({
   user,
-  recipe
+  recipe,
+  singleRecipe
 })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
