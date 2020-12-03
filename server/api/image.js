88ffac2bfc4e23275746593425dd31a9ce3bc103
Lidia De La Cruz <lidcruz@hotmail.com>
2020-12-03 18:31:11 -0500

    working on Image Uploader

--- server/api/image.js
+++ server/api/image.js
@@ -1,5 +1,34 @@
 const router = require('express').Router()
-const {Image} = require('../db/models')
+const {Image} = require('../db/models/image')
 const multer = require('multer')
 
 module.exports = router
+//hanldes image uploading using multer
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + file.originalname)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true)
+  } else {
+    cb(null, false)
+    // const err = new Error('Only JPEG or PNG format')
+    //   err.status = 401
+    //   return next(err)
+  }
+}
+//check to see if the commented out code works
+
+const upload = multer({
+  storage: Storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+})
