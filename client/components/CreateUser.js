d046d823ef9c7e137c71bcbe789adfe21fa475d0
Kade Cahe <55356085+Kadecahe@users.noreply.github.com>
2020-12-03 18:07:00 -0500

    Update CreateUser.js

--- client/components/CreateUser.js
+++ client/components/CreateUser.js
@@ -1,4 +1,4 @@
-import React, {Component, Profiler} from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {postUser} from '../store/user'
 import {auth} from '../store'
