9fa67c695eb31dec30a9cd6ffa82710c4861b602
Kadecahe <kadecahe@gmail.com>
2020-12-06 04:27:32 -0500

    Feat: Completed CRUD for All Channels

--- client/components/edit-channel-sections/add-channel.js
+++ client/components/edit-channel-sections/add-channel.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import {Button, Form} from 'react-bootstrap'
+import {connect} from 'react-redux'
+import {createChannel} from '../../store/channel'
 
 class AddChannel extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      deleteCheckbox: false
+      deleteCheckbox: false,
+      name: '',
+      description: '',
+      imageUrl:
+        'https://mzo5g3ubj8u20bigm1x3cth1-wpengine.netdna-ssl.com/wp-content/uploads/2017/05/Eat-this-to-build-muscle-M-610x407.jpg',
+      // private: false,
+      isPrivate: false
     }
 
     this.handleChange = this.handleChange.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
   componentDidMount() {
-    console.log('delete channel component')
+    console.log('add channel component')
   }
 
   handleChange() {
@@ -20,44 +29,123 @@ class AddChannel extends React.Component {
     this.setState({deleteCheckbox: !bool})
   }
 
+  handleInputChange(event) {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
   handleSubmit(event) {
     event.preventDefault()
-    this.props.deleteChannel(this.props.channel.id)
-    this.props.handleClose()
+    let {name, description, imageUrl, isPrivate} = this.state
+    this.props.createChannel({name, description, isPrivate, imageUrl})
+    this.props.close()
   }
 
   render() {
-    if (!this.props.channel) return <h1>Loading</h1>
+    let makePrivate = this.state.isPrivate
+
     return (
       <Form onSubmit={this.handleSubmit}>
-        <Form.Group controlId="deleteCheckbox" onChange={this.handleChange}>
-          <Form.Label className>
-            Are you sure you want to delete {this.props.channel.name}? All the
-            users and channel details will be removed from WhatsCookin
-            immedietly. Recipes, however, will not be deleted for individual
-            users. This action cannot be undone.{' '}
+        <Form.Group className="mb-0">
+          <Form.Label>
+            Channels are spaces where you can share and explore recipes. They’re
+            best when focused on a type of cuisine, diatary restriction, or a
+            friend group.
           </Form.Label>
-          <Form.Check
-            type="checkbox"
-            label="Yes, permanently delete this channel"
-            name="deleteCheckbox"
+        </Form.Group>
+        <Form.Group controlId="name" className="mb-1 mt-1">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            name="name"
+            type="name"
+            placeholder="e.g Family Recipes"
+            value={this.state.name}
+            onChange={this.handleInputChange}
           />
-          <Button variant="outline-secondary" onClick={this.props.handleClose}>
-            Cancel
-          </Button>{' '}
-          {this.state.deleteCheckbox ? (
-            <Button variant="danger" type="submit" size="sm">
-              Delete Channel
-            </Button>
+          <Form.Text className="text-muted">
+            Names can’t be longer than 80 characters.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId="description" className="mb-1 mt-1">
+          <Form.Label>
+            Description <span className="text-muted">(Optional)</span>
+          </Form.Label>
+          <Form.Control
+            name="description"
+            type="description"
+            value={this.state.description}
+            onChange={this.handleInputChange}
+          />
+          <Form.Text className="text-muted">
+            Who are these recipes for?
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="imageUrl" className="mb-1 mt-1">
+          <Form.Label>
+            Channel Image
+            <span className="text-muted">(Optional)</span>
+          </Form.Label>
+          {/* edit images width and find a way to store it */}
+          {/* {' '} */}
+          <br />
+          <Form.File
+            id="imageUpload"
+            name="imageUrl"
+            className="m-0 mb-1"
+            // onChange={this.handleChange}
+          />
+          <br />{' '}
+        </Form.Group>
+        <Form.Group
+          controlId="isPrivate"
+          onChange={() => this.setState({isPrivate: !makePrivate})}
+          className="mb-1 mt-1"
+        >
+          <Form.Label>Make Private</Form.Label>
+          {this.state.isPrivate ? (
+            <div className="d-flex flex-nowrap justify-items-between m-0 align-items-center">
+              <Form.Label className="text-muted">
+                This can’t be undone. A private channel cannot be made public
+                later on.
+              </Form.Label>
+              <Form.Check
+                type="checkbox"
+                label="Private?"
+                name="isPrivate"
+                value={this.state.name}
+                onChange={this.handleInputChange}
+              />
+            </div>
           ) : (
-            <Button variant="danger" type="submit" size="sm" disabled>
-              Delete Channel
-            </Button>
+            <div className="d-flex flex-nowrap justify-items-between m-0 align-items-center">
+              <Form.Label className="text-muted">
+                When a channel is set to private, it can only be viewed or
+                joined by invitation.
+              </Form.Label>
+              <Form.Check type="checkbox" label="Private?" name="isPrivate" />
+            </div>
           )}
         </Form.Group>
+        {this.state.name ? (
+          <div className="d-flex justify-content-end mr-4 mt-1">
+            <Button variant="info" type="submit">
+              Create
+            </Button>
+          </div>
+        ) : (
+          <div className="d-flex justify-content-end mr-4 mt-1">
+            <Button variant="info" type="submit" disabled>
+              Create
+            </Button>
+          </div>
+        )}
       </Form>
     )
   }
 }
 
-export default AddChannel
+const mapDispatch = dispatch => ({
+  createChannel: channel => dispatch(createChannel(channel))
+})
+
+export default connect(null, mapDispatch)(AddChannel)
