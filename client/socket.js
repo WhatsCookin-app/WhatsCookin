049431dbb0744ecfff7e20efccc2b6332efbff13
Kadecahe <kadecahe@gmail.com>
2020-12-06 22:30:01 -0500

    Feat: added constraints

--- client/socket.js
+++ client/socket.js
@@ -2,6 +2,23 @@ import io from 'socket.io-client'
 
 const socket = io(window.location.origin)
 
+let localStream
+let isCaller = false
+let rtcPeerConnection
+
+const iceServers = {
+  iceServers: [
+    {urls: 'stun:stun.services.mozilla.com'},
+    {urls: 'stun:stun.l.google.com:19302'}
+  ]
+}
+
+const constraints = {
+  video: {facingMode: 'user'},
+  // Uncomment to enable audio
+  audio: true
+}
+
 socket.on('connect', () => {
   console.log('Connected!')
 })
